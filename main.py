3b2df1c1346c3624efc20c67aea617399ae27da4
Kunakbaev <53218382+Kunakbaev@users.noreply.github.com>
2022-04-23 23:29:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import setup
-import getDataFromDatabase as db
 from app import MainApp
-import solve
 
 if __name__ == '__main__':
-
-    nouns = db.getData()
-    print('Nouns have been loaded : ', len(nouns))
-
     setup.loadNouns()
-    solve.loadNounsToDll()
-    # print(meanings)
-    # print(nouns)
-
     app = MainApp()
     app.run()
